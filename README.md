464becfcbe64fb2072d34c4e42d7fa25b461b18b
sumon paul <sumpaul100@gmail.com>
2023-11-24 01:54:44 +0600

    project environment setup without env

---
+++ README.md
@@ -0,0 +1,2 @@
+
+Client Side repo: https://github.com/programming-hero-web-course1/b8a12-client-side-sumonpaul55
---
+++ src/router/Router.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+    createBrowserRouter,
+} from "react-router-dom";
+import MainLayout from '../layout/MainLayout';
+import Home from '../pages/home/Home';
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <MainLayout></MainLayout>,
+        children: [
+            {
+                path: "/",
+                element: <Home></Home>
+            }
+        ]
+    },
+]);
+
+
+
+export default router;
