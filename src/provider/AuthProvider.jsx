1b9f071a5ed45f81259fc2278badece5f58455e0
sumon paul <sumpaul100@gmail.com>
2023-11-28 04:14:24 +0600

    jwt implement

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,12 +1,14 @@
 import React, { createContext, useEffect, useState } from 'react';
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
 import app from '../firebase/firebase.config';
+import useAxiosPublic from '../hook/useAxiosPublic';
 export const AuthContext = createContext(null)
 const auth = getAuth(app)
 
 const AuthProvider = ({ children }) => {
     const [loading, setloading] = useState(true)
     const [user, setUser] = useState(null)
+    const axiosPublic = useAxiosPublic()
     const googleProvider = new GoogleAuthProvider();
     const signUp = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password)
@@ -22,8 +24,25 @@ const AuthProvider = ({ children }) => {
         return signOut(auth)
     }
     useEffect(() => {
-        const unSubcribe = onAuthStateChanged(auth, cureentUser => {
-            setUser(cureentUser)
+        const unSubcribe = onAuthStateChanged(auth, curentUser => {
+            setUser(curentUser)
+            const userInfo = {
+                email: setUser?.email
+            }
+            if (curentUser) {
+                // assign get token store client
+                axiosPublic.post("/jwt", userInfo)
+                    .then(res => {
+                        if (res.data.token) {
+                            localStorage.setItem("access-token", res.data.token)
+                            setloading(false)
+                        }
+                    })
+            } else {
+                //TODO: remove token (if token stored in the client side: local storage, chaching, in memory)
+                localStorage.removeItem("access-token")
+                setloading(false)
+            }
             setloading(false)
         })
 
