454c3603bfe9e4554194f83d686160dba5d19d46
sumon paul <sumpaul100@gmail.com>
2023-11-30 19:50:47 +0600

    error page

--- src/shared/Error.jsx
+++ src/shared/Error.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
-    return (
-        <div>
 
+    return (
+        <div className='min-h-screen bg-slate-500 pt-32'>
+            <div className='border max-w-[600px] mx-auto p-10 rounded-lg text-center'>
+                <h2 className='font-bold text-xl'>404 Page Not Found</h2>
+                <div className='mt-20'>
+                    <Link to="/" className='hover:text-pink-600 font-bold'>Home</Link>
+                </div>
+            </div>
         </div>
     );
 };
